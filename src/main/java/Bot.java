2fedb2c19f76094ff623f70b3a18c8f259e2502a
Vitalii Vernihor <43929128+Taska23@users.noreply.github.com>
2020-06-12 22:06:26 +0300

    Update Bot.java

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,5 +1,6 @@
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.groupadministration.RestrictChatMember;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
@@ -8,6 +9,9 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
 
+import java.security.Permission;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Bot extends TelegramLongPollingBot {
@@ -22,6 +26,8 @@ public class Bot extends TelegramLongPollingBot {
         }
     }
 
+
+
     public void sendMsg(Message message, String text){
         SendMessage sendMessage = new SendMessage();
         sendMessage.enableMarkdown(true);
@@ -34,6 +40,42 @@ public class Bot extends TelegramLongPollingBot {
         }
     }
 
+    public void unMuteUser(Message message, int id){
+        RestrictChatMember restrictChatMember = new RestrictChatMember();
+        restrictChatMember.setChatId(message.getChatId());
+        restrictChatMember.setUserId(id);
+        restrictChatMember.setCanSendMessages(true);
+        restrictChatMember.setCanSendMediaMessages(true);
+        restrictChatMember.setCanSendOtherMessages(true);
+        sendMsg(message,"Вроде получилось");
+        try {
+            execute(restrictChatMember);
+        }catch (TelegramApiException e){
+            e.printStackTrace();
+            sendMsg(message," ох блять, эксепшон вывалился");
+
+        }
+    }
+
+    public void muteUser(Message message, int id){
+        RestrictChatMember restrictChatMember = new RestrictChatMember();
+        restrictChatMember.setChatId(message.getChatId());
+        restrictChatMember.setUserId(id);
+        restrictChatMember.setCanSendMessages(false);
+        restrictChatMember.setCanSendMediaMessages(false);
+        restrictChatMember.setCanSendOtherMessages(false);
+        restrictChatMember.setUntilDate((int) ((System.currentTimeMillis() / 1000L) + 900));
+        try {
+            execute(restrictChatMember);
+        }catch (TelegramApiException e){
+            e.printStackTrace();
+            sendMsg(message," ох блять, эксепшон вывалился");
+
+        }
+    }
+
+
+
     public void onUpdateReceived(Update update) {
 
         Message message = update.getMessage();
@@ -43,8 +85,34 @@ public class Bot extends TelegramLongPollingBot {
             String text = message.getText();
             System.out.println(message);
 
+            if (counter !=0 && System.currentTimeMillis() - startTime > 600000){
+                counter = 0;
+                muteActivators[0] = 0;
+                muteActivators[1] = 0;
+                muteActivators[2] = 0;
+                muteActivators[3] = 0;
+                muteActivators[4] = 0;
+                idOfMutedUser = 0;
+                nameOfMutedUser = "";
+                sendMsg(message, "Сбросил счётчик");
+            }
 
+            if (text.contains("/")){
+                if(lastUserArr(message.getFrom())){
+                    sendMsg(message,"Кто-то наспамил много команд, пока что дебаг, но в будущем бан нахуй");
+                    muteUser(message, message.getFrom().getId());
+                }
+            }
+            if (text.contains("/unmute") && message.isReply() && message.getFrom().getId().equals(138368804)){
+                sendMsg(message, "пробую разбанить");
+                unMuteUser(message, message.getReplyToMessage().getFrom().getId());
 
+            }
+            if (text.contains("/ultimatemute") && message.isReply() && message.getFrom().getId().equals(138368804)){
+                sendMsg(message, "пизда воробушку");
+                muteUser(message, message.getReplyToMessage().getFrom().getId());
+
+            }
             if (text.contains("Вася") || text.contains("вася")) {
                // sendMsg(message, "Вася, ну йоханый манус");
             } else if (text.contains("Манус") || text.contains("манус")) {
@@ -55,12 +123,106 @@ public class Bot extends TelegramLongPollingBot {
                 sendMsg(message, randomManusGenerator());
             }else if ("/manus_kanon".equalsIgnoreCase(text) || "/manus_kanon@yxrix_bot".equalsIgnoreCase(text)){
                 sendMsg(message, randomManusKanon());
-            }//else if(message.getFrom().getId().equals(407449415) && text.contains("?")){
-               // sendMsg(message, "Вася, ну йоханый манус, блять!");
-            //}
+            }
+            if(("/voteban".equalsIgnoreCase(text) || "/voteban@yxrix_bot".equalsIgnoreCase(text)) && message.isReply()){
+                if (counter == 0) {
+                    sendMsg(message, "Инициализация мута на 15 минут для " + message.getReplyToMessage().getFrom().getFirstName() + "\n @" + message.getReplyToMessage().getFrom().getUserName() + " Если молодые наберут 5/5 то ты получишь мут на 15 минут");
+                    nameOfMutedUser = (message.getReplyToMessage().getFrom().getFirstName() + " @" + message.getReplyToMessage().getFrom().getUserName());
+                    idOfMutedUser = message.getReplyToMessage().getFrom().getId();
+                    muteActivators[counter] = message.getFrom().getId();
+                    counter++;
+                    startTime = System.currentTimeMillis();
+                }else {
+                    if (isVoted(message.getFrom().getId())){
+                        sendMsg(message,"Ты уже голосовал");
+                    }else {
+                        muteActivators[counter] = message.getFrom().getId();
+                        counter++;
+                        sendMsg(message,"Засчитал. " + counter + "/5");
+                    }
+
+                }
+                if (counter == 5){
+                    sendMsg(message,nameOfMutedUser + " Отправляется в мут на 15 минут.");
+                    muteUser(message, idOfMutedUser);
+                    counter = 0;
+                    muteActivators[0] = 0;
+                    muteActivators[1] = 0;
+                    muteActivators[2] = 0;
+                    muteActivators[3] = 0;
+                    muteActivators[4] = 0;
+                }
+            }else if ("/voteban".equalsIgnoreCase(text) || "/voteban@yxrix_bot".equalsIgnoreCase(text)){
+                if (counter == 0) {
+                    sendMsg(message, "Ответь этой командой на сообщение того, кого хочешь забанить нахуй");
+                }else {
+                    if (isVoted(message.getFrom().getId())){
+                        sendMsg(message,"Ты уже голосовал");
+                    }else {
+                        muteActivators[counter] = message.getFrom().getId();
+                        counter++;
+                        sendMsg(message,"Засчитал. " + counter + "/5");
+                    }
+
+                }
+                if (counter == 5){
+                    sendMsg(message,nameOfMutedUser + " Отправляется в мут на 15 минут.");
+                    counter = 0;
+                    muteActivators[0] = 0;
+                    muteActivators[1] = 0;
+                    muteActivators[2] = 0;
+                    muteActivators[3] = 0;
+                    muteActivators[4] = 0;
+                    muteUser(message, idOfMutedUser);
+                }
+            }
+            if("/yesno".equalsIgnoreCase(text) || "/yesno@yxrix_bot".equalsIgnoreCase(text)){
+                sendMsg(message,yesNo());
+            }
+
+            //else if(message.getFrom().getId().equals(407449415)){
+              // sendMsg(message, "Ладно не будем спорить на счет российского языка");
+            // }
         }
 
     }
+
+    public String yesNo (){
+        Random random = new Random();
+        if (random.nextInt(1) == 0){
+            return "Да.";
+        } else {
+            return "Нет.";
+        }
+    }
+
+    public boolean isVoted (int id){
+        boolean result = false;
+        for (int i = 0; i < muteActivators.length;i++){
+            if (muteActivators[i] == id){
+                result = true;
+            }
+        }
+        return result;
+    }
+    int lastUserArray[] = new int[5];
+    public boolean lastUserArr(User user){
+        lastUserArray[0]=lastUserArray[1];
+        lastUserArray[1]=lastUserArray[2];
+        lastUserArray[2]=lastUserArray[3];
+        lastUserArray[3]=lastUserArray[4];
+        lastUserArray[4]=user.getId();
+
+
+        if(lastUserArray[1] == user.getId() && lastUserArray[2] == user.getId() && lastUserArray[3] == user.getId() && lastUserArray[4] == user.getId()){
+            return true;
+        } else return false;
+    }
+    int counter = 0;
+    int muteActivators[] = new int[5];
+    String nameOfMutedUser;
+    int idOfMutedUser;
+    long startTime;
 
     public String randomManusGenerator(){
 
